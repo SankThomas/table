ded470cd0927ee9d4ab20c41e7f79844c8d90a18
Thomas Sankara <thomassankara74@gmail.com>
2021-03-26 21:06:03 +0300

    initial commit

---
+++ src/AddButton.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Button } from "@material-ui/core"
+
+const AddButton = () => {
+  return (
+    <>
+      <div>
+        <Button color="primary">Add Row</Button>
+      </div>
+    </>
+  )
+}
+
+export default AddButton
--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import AddButton from "./AddButton"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section>
+        <div style={{ marginBottom: 10 }}>
+          <AddButton />
+        </div>
+        <table>
+          <tr>
+            <th>Task Name</th>
+            <th>Linked Status</th>
+            <th>Start After (Number of Days)</th>
+            <th>Document Mandatory?</th>
+            <th>Auto Allocation?</th>
+          </tr>
+          <tr>
+            <td>
+              <select>
+                <option value="1" selected>
+                  One
+                </option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </td>
+            <td>
+              <select>
+                <option value="1" selected>
+                  Four
+                </option>
+                <option value="2">Five</option>
+                <option value="3">Six</option>
+              </select>
+            </td>
+            <td>
+              <input
+                type="number"
+                name="number"
+                id="number"
+                placeholder="Enter days"
+              />
+            </td>
+            <td>
+              {/* Toggle button */}
+              <div class="toggle">
+                <input type="checkbox" class="checkbox" id="checkbox" />
+
+                <label for="checkbox" class="label">
+                  <div class="ball"></div>
+                </label>
+              </div>
+              {/* End of toggle button  */}
+            </td>
+            <td>
+              {/* Toggle button */}
+              <div class="toggle">
+                <input type="checkbox" class="checkbox" id="checkbox2" />
+
+                <label for="checkbox2" class="label">
+                  <div class="ball"></div>
+                </label>
+              </div>
+              {/* End of toggle button  */}
+            </td>
+          </tr>
+        </table>
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
